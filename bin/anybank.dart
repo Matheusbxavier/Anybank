9c7ffe52d5a63b4cd865df1210a58e4898822624
Matheus Batista Xavier <101474699+Matheusbxavier@users.noreply.github.com>
2025-09-04 12:58:21 -0300

    commit

---
+++ bin/anybank.dart
@@ -0,0 +1,38 @@
+void main() {
+  Conta contaMatheus = Conta("Matheus", 1000);
+  Conta contaRoberta = Conta("Roberta", 2000);
+
+  List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
+
+  for (Conta conta in contas) {
+    print(conta.titular);
+    print(conta.saldo);
+  }
+
+  contaRoberta.saldo = 5000;
+  print(contaRoberta.saldo);
+
+  contaMatheus.receber(500);
+
+  print(contaMatheus.titular);
+  print(contaMatheus.saldo);
+
+  contaMatheus.enviar(200);
+  print(contaMatheus.titular);
+  print(contaMatheus.saldo);
+}
+
+class Conta {
+  String titular;
+  double saldo;
+
+  Conta(this.titular, this.saldo);
+
+  void receber(double valor) {
+    saldo += valor;
+  }
+
+  void enviar(double valor) {
+    saldo -= valor;
+  }
+}
